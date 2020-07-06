d8e355f00093b9eea7d09430a5bf835dc31d5115
Zoey <zoey@panties.moe>
2020-07-05 20:25:27 -0400

    Added support for Twitter for Android keys.

--- lib/api.dart
+++ lib/api.dart
@@ -1,12 +1,14 @@
 /// Twitter API wrapper to make simple request to Twitter's servers
 /// @ZoeyLovesMiki, 2020
 
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:device_info/device_info.dart';
 import 'package:twitter_client/auth.dart'; 
 
 const BASE_URL = "api.twitter.com";
 
-/// TwitterAPI
+/// Wrapper class to call the Twitter API doing raw requests
 class TwitterAPI {
   String consumerKey;
   String consumerSecret;
@@ -20,13 +22,16 @@ class TwitterAPI {
     this.tokenSecret = ""
   });
 
+  /// Call the Twitter API with proper oauth headers and signature
+  /// [method] is the HTTP method to use, GET or POST.
+  /// [url] is the full path for the endpoint, /1.1/some/thing.json
   Future<http.Response> _request(String method, String url, Map<String, String> params, {Map<String, String> body}) async {
     if (params == null) params = {};
 
     Uri originalUrl = Uri.parse("https://$BASE_URL$url");
-
     Uri finalUrl = _fixedURL(url, params);
     
+    // Create our OauthHelper instance
     OAuthHelper helper = new OAuthHelper(
       consumerKey: this.consumerKey,
       consumerSecret: this.consumerSecret,
@@ -34,14 +39,24 @@ class TwitterAPI {
       tokenSecret: this.tokenSecret
     );
 
+    if (body != null) {
+      params.addAll(body);
+    }
+
+    // Create our oAuth signature and Authorization header
     String signature = helper.getSignature(method, originalUrl.toString(), params);
     String oauthHeader = helper.getAuthHeader(signature);
 
+    // Set the proper headers
     Map<String, String> headers = {
       "Authorization": oauthHeader,
       "Content-Type": "application/x-www-form-urlencoded"
     };
 
+    // Add extra heders if needed
+    headers.addAll(await _getExtraHeaders(helper.type));
+
+    // Do our http request and return the response
     http.Response response;
 
     if (method == "POST") {
@@ -51,6 +66,49 @@ class TwitterAPI {
     }
 
     return response;
+  }
+
+  /// Get extra headers
+  Future<Map<String, String>> _getExtraHeaders(KeyType keyType) async {
+    Map<String, String> extraHeaders = new Map();
+
+    // Twitter for Android requires extra headers to make the request works using Twitter's keys
+    if (keyType == KeyType.Android) {
+      const String clientName = "TwitterAndroid";
+      const String versionName = "6.41.0";
+      const String apiVersion = "5";
+      const String internalVersionName = "7160062-r-930";
+
+      // Default values used when using the keys on a non Android platform
+      String model = "Pixel 3";
+      String sdkVersion = "30";
+      String manufacturer = "Google";
+      String brand = "google";
+      String product = "blueline";
+
+      // Use the actual device information if we're using an Android phone
+      if (Platform.isAndroid) {
+        DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+
+        model = androidInfo.model;
+        sdkVersion = androidInfo.version.sdkInt.toString();
+        manufacturer = androidInfo.manufacturer;
+        brand = androidInfo.brand;
+        product = androidInfo.product;
+      }
+
+      extraHeaders.addAll({
+        "User-Agent": "$clientName/$versionName ($internalVersionName) $model/$sdkVersion ($manufacturer;$model;$brand;$product;0;;0)",
+        "Accept-Language": "en_US",
+        "X-Twitter-Client": clientName,
+        "X-Twitter-Client-Language": "en_US",
+        "X-Twitter-Client-Version": versionName,
+        "X-Twitter-API-Version": apiVersion
+      });
+    }
+
+    return extraHeaders;
   }
 
   // Twitter's oAuth is garbage
@@ -70,11 +128,13 @@ class TwitterAPI {
     return Uri.https("api.twitter.com", urlWithoutQuery, paramsWithQuery);
   }
 
+  /// Do a post request to [url] using the [params] and [body]
   Future<http.Response> post(String url, {Map<String, String> params, Map<String, String> body}) async{
     if (body == null) body = new Map();
     return await _request("POST", url, params, body: body);
   }
 
+  /// Do a get request to [url] using the [params] and [body]
   Future<http.Response> get(String url, {Map<String, String> params}) async {
     return await _request("GET", url, params);
   }
