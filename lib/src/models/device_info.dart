59f53b55ef88db28523989eaddbd4a23966af9f1
Zoey <zoey@panties.moe>
2021-11-13 17:37:44 -0500

    model to set the device information

---
+++ lib/src/models/device_info.dart
@@ -0,0 +1,15 @@
+class DeviceInfo {
+  String model;
+  String sdkVersion;
+  String manufacturer;
+  String brand;
+  String product;
+
+  DeviceInfo({
+    this.model = "Pixel 3",
+    this.sdkVersion = "30",
+    this.manufacturer = "Google",
+    this.brand = "google",
+    this.product = "blueline",
+  });
+}
