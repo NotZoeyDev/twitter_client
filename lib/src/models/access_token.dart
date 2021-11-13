8c29f39beea9fd3fbaca3213f7706b69ad087462
Zoey <zoey@panties.moe>
2021-11-13 12:35:23 -0500

    endpoints and renames

--- lib/src/models/access_token.dart
+++ lib/src/models/access_token.dart
@@ -1,17 +1,17 @@
 class AccessToken {
-  String? token;
-  String? tokenSecret;
-  String? screenName;
+  String token;
+  String tokenSecret;
+  String screenName;
 
-  AccessToken({
+  AccessToken(
     this.token,
     this.tokenSecret,
     this.screenName
-  });
+  );
 
   factory AccessToken.fromJson(Map<String, dynamic> json) => AccessToken(
-    token: json['oauth_token'],
-    tokenSecret: json['oauth_token_secret'],
-    screenName: json['screen_name']
+    json['oauth_token'],
+    json['oauth_token_secret'],
+    json['screen_name'],
   );
 }
--- lib/src/models/request_token.dart
+++ lib/src/models/request_token.dart
@@ -1,14 +1,14 @@
 class RequestToken {
-  String? oauthToken;
-  String? oauthTokenSecret;
+  String oauthToken;
+  String oauthTokenSecret;
 
-  RequestToken({
+  RequestToken(
     this.oauthToken,
     this.oauthTokenSecret
-  });
+  );
 
   factory RequestToken.fromJson(Map<String, dynamic> json) => RequestToken(
-    oauthToken: json['oauth_token'],
-    oauthTokenSecret: json['oauth_token_secret']
+    json['oauth_token'],
+    json['oauth_token_secret'],
   );
 }
