bc579b4ca426f04ff24ea43124b1119eccc8d757
Zoey <zoey@panties.moe>
2021-11-19 16:57:16 -0500

    responses for media endpoints

--- lib/src/responses/media.dart
+++ lib/src/responses/media.dart
@@ -5,6 +5,7 @@ class MediaUpload {
   int? size;
   int? expiresAfterSecs;
   _MediaUploadImage? image;
+  _MediaUploadInfo? processingInfo;
   
   MediaUpload(
     this.mediaID,
@@ -12,7 +13,8 @@ class MediaUpload {
     this.mediaKey,
     this.size,
     this.expiresAfterSecs,
-    this.image
+    this.image,
+    this.processingInfo,
   );
 
   factory MediaUpload.fromJson(Map<String, dynamic> json) => MediaUpload(
@@ -21,7 +23,8 @@ class MediaUpload {
     json['media_key'],
     json['size'],
     json['expires_after_secs'],
-    _MediaUploadImage.fromJson(json['image'])
+    json['image'] != null ? _MediaUploadImage.fromJson(json['image']) : null,
+    json['processing_info'] != null ? _MediaUploadInfo.fromJson(json['processing_info']) : null,
   );
 }
 
@@ -36,5 +39,35 @@ class _MediaUploadImage {
     json['image_type'],
     json['w'],
     json['h']
+  );
+}
+
+class _MediaUploadInfo {
+  String? state;
+  int? checkAfterSecs;
+  int? progressPercent;
+  _MediaUploadInfoError? error;
+
+  _MediaUploadInfo(this.state, this.checkAfterSecs, this.progressPercent, this.error);
+
+  factory _MediaUploadInfo.fromJson(Map<String, dynamic> json) => _MediaUploadInfo(
+    json['state'],
+    json['check_after_secs'],
+    json['progress_percent'],
+    _MediaUploadInfoError.fromJson(json['error']),
+  );
+}
+
+class _MediaUploadInfoError {
+  int? code;
+  String? name;
+  String? message;
+
+  _MediaUploadInfoError(this.code, this.name, this.message);
+
+  factory _MediaUploadInfoError.fromJson(Map<String, dynamic> json) => _MediaUploadInfoError(
+    json['code'],
+    json['name'],
+    json['message']
   );
 }
